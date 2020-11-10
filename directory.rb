3ece3034e2c3c5724e8b2617143867db5dc4c321
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-10 15:46:40 +0000

    Add comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# Finally we print the total number of students
 print "Overall, we have "
+# It's important that print() doesn't add new line characters
 print 11
 puts " great students"
