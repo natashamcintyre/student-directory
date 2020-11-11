f48e50020c9e96ee6b06b8681aa1ff473baf062c
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-11 11:06:20 +0000

    Center improves output presentation

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -5,7 +5,7 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.split.map(&:capitalize).join(' ')
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
@@ -18,15 +18,17 @@ def input_students
     }
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.split.map(&:capitalize).join(' ')
   end
   # return the array of students
   students
 end
 # print student summary
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  @title1 = "The students of Villains Academy"
+  puts @title1
+  # 6. use center to improve output presentation
+  puts "-------------".center(@title1.length)
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
@@ -35,7 +37,7 @@ end
 # 1. print students with number in front of name
 def print(students)
   students.each_with_index do |student,index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(@title1.length)
   end
 end
 # 2. prints students whose name begins with certain letter
@@ -63,14 +65,14 @@ end
 def printloop(students)
   i = 0
   while i < students.count
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)".center(@title1.length)
     i += 1
   end
 end
 #
 
 students = input_students
-# print_header
+print_header
 # print(students)
 # print_short(students)
 # print_footer(students)
